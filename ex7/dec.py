6a170330ca303f7e0af5c8680bfc09a3aeec12dc
root <ricardojruiz93@gmail.com>
2019-03-16 00:46:50 +0100

    Ex7 - Added Python encoder-decoder

---
+++ ex7/dec.py
@@ -0,0 +1,22 @@
+import os, sys
+from crypto import *
+from ast import literal_eval
+
+aes_key = 'r'*16
+pkeyname="private.pem"
+
+
+def create_key():
+	f= open(pkeyname,"w+")
+	f.write('-----BEGIN RSA PRIVATE KEY-----\nMIIEowIBAAKCAQEAtS5K6YGROGa01x8HhPo2kaKn+Gme3ihCI7dieh09iaAphXbj\nhNsKuE309J8nFZKecuImfQt8bERmerd9ndommIQfJSY9iCzhEOhL5xsSOfqEzFaY\nBTcRdpgKNQBwk7BCsRjSuN8MPvuwaPKG9FxFTOo5lEGz+lFUlhKeVk8YFl8KQCr7\nRNleQ4I+ZHp/3qWWNlD5CGY5O7h7H2j6YlgwDSCmx4yO0xVEV0OHAZkyM4zZzay0\n3/bfYpcnmWUrN2nWlRfHQ6h1uIjjCo5Xafmr6veSASe95V+9uU9FOGFrJggDQSX9\nzLzJfzUoqla429Zd6D4h2zIf0Oyu7srxRBQxIQIDAQABAoIBAQCiaB6qdYQxJI+S\nDbR11Lw5i645lKPdzASNc4MIp1FUHodZpZqIqrhPQUIETO2B/u2dWW7axAMCIcYi\n1nv5lWTnbfdlG4QonuIYf8IaJUAPTKrhZ/XZJuW2gs8Td5NyL5t4mCG7qeSWSJJ3\nYH7saBGOwp5ecQ7doS3LYGEiSL/0vDaaAl5+ZLPP2Tv/c1GpUcg7FMx3KDTrqDJM\n5iyDSqGByrDPFcxNZWwxYQJ88jWsOJkmgD8zKmXsMYZtf7uOrKykKNKAG8ROU06w\n38zidYEGgWaE/TkgJKLp53BH3mmFwhsTRmKQmKn/J1+YrC7C+r3CCC66iZZ2eYjZ\nygDmuN7hAoGBANmg4pdCMstKUlShDkYODMJ+GhcQTw6vUS8lGy5cXCHtPoy5IPbn\n9Zbx2edsb3kavq5z+r4wb/rtuNa6ccb76HI5UZtBojpCyrMKNYvOX8W4rlzpR8Ss\n0IIf3UoCztpPJKSN3mGoXASgrPLA1HEH4+hnBvMDNvOK9lMhoDN9X/r1AoGBANUg\nRIuBcDkzk4yb+IFmq99Q7m7Nh6OkW4W4x18yBxXjYSLXrzO/BA9LMPA8aP6pezNd\nI/Iw5r/Sn5IF/tSSw43kkyGSSCv9Up3VSBAKarl6K2DngUvze0mEAyeDWu/+jdCo\nct/SghF852gWRcnx8HODpb6M2i4y+uKHQ9j90ln9AoGAew09EcmcbtXXN4LZT0Mi\nc9ZZajEOgn8eqEmS1OPkNEqw02DaAsGlW3PkgbYOUauNpZZucJtyta80lDVXn+tP\nw0YlnmO+FrMtaY6cODC4dlO2Q1KkAdC1FN5vgSmVTFrznReaZh2L6hHvuFLPzBlq\nBD4876shpxMrtFWO/44kdwECgYBCN5ZUadD+VMIBK+yX99hqt/B8yKrd8xDkiTaY\noyiEanMhqjWrNCwEvG3mFj7g8LNwj/29H+hxrPUbH3W/giH0EhKMmbCGylxC4aNU\nKIhkPEsB/quzd2DUxIleW3eDnWlebRfT5sm2uUIEmvbv+ZWlrj9u5sBbHkfI3hH8\nd1BF2QKBgAVubCpU7CgiwkMhEGRSALsuYh2Lsny/UW3bB0hd6EhPHP9sYf/DVyfF\n8t7Ewv+bzDIN5XmXCszNLF7YzHNNJhz9fLPO8L8kNML50RTh297ujwpUK5pXUoUe\n7qStmd+Z9lONVf6ZAcBQhIZeLQoQD+pcO9OuVe4sEoJOdWI7ANVv\n-----END RSA PRIVATE KEY-----\n')
+	f.close()
+
+
+def decrypt_(message):
+	key = 'r'*16
+	b = (RSACipher().decrypt(message))
+	dec_message = AESCipher(key).decrypt(b)
+	return dec_message
+
+dec_message = decrypt(sys.argv[1])
+print dec_message
---
+++ ex7/main.py
@@ -0,0 +1,103 @@
+import os,sys
+import base64
+from Crypto.Cipher import AES
+import Crypto
+from ctypes import CDLL, c_char_p, c_void_p, memmove, cast, CFUNCTYPE
+import argparse
+
+def get_args():
+  parser = argparse.ArgumentParser()
+  parser.add_argument('-k', '--aes_key', required=True, action='store', help='AES Key')
+  parser.add_argument('-e', '--encrypt', required=False, action='store', help='Encrypt')
+  parser.add_argument('-d', '--decrypt', required=False, action='store', help='Decrypt')
+  parser.add_argument('-x', '--execute', required=False, action='store_true', help='Execute the decoded shellcode')
+  my_args = parser.parse_args()
+  return my_args
+
+
+BS = 16
+pad = lambda s: s + (BS - len(s) % BS) * chr(BS - len(s) % BS).encode()
+unpad = lambda s: s[:-ord(s[len(s)-1:])]
+
+def iv():
+	return chr(0) * 16
+
+
+class AESCipher(object):
+
+	def __init__(self, key):
+		self.key = key
+
+	def encrypt(self, message):
+		message = message.encode()
+		raw = pad(message)
+		cipher = AES.new(self.key, AES.MODE_CBC, iv())
+		enc = cipher.encrypt(raw)
+		return base64.b64encode(enc).decode('utf-8')
+
+	def decrypt(self, enc):
+		enc = base64.b64decode(enc)
+		cipher = AES.new(self.key, AES.MODE_CBC, iv())
+		dec = cipher.decrypt(enc)
+		return unpad(dec).decode('utf-8')
+
+
+def encrypt_(message, aes_key):
+	enc_message = AESCipher(aes_key).encrypt(message)
+	return enc_message
+
+
+def decrypt_(message, aes_key):
+	dec_message = AESCipher(aes_key).decrypt(message)
+	return dec_message
+
+
+# http://hacktracking.blogspot.com/2015/05/execute-shellcode-in-python.html
+def run_shellcode(shellcode):
+	libc = CDLL('libc.so.6')
+	shellcode = shellcode.replace('\\x', '').decode('hex')
+	sc = c_char_p(shellcode)
+	size = len(shellcode)
+	addr = c_void_p(libc.valloc(size))
+	memmove(addr, sc, size)
+	libc.mprotect(addr, size, 0x7)
+	run = cast(addr, CFUNCTYPE(c_void_p))
+	print "\nExecuting shellcode...\n"
+	run()
+
+
+def show_info(text, shellcode=False):
+	if shellcode:
+		text_size = len([letter for letter in text if letter == 'x'])
+		print "\nShellcode Length: %s" % (text_size)	
+		print "Shellcode: %s" % (text)	
+		print "\n------------------------------"
+	else:
+		print "\nEncoded Shellcode: %s" % (text)	
+		print "\n------------------------------"
+
+
+
+
+def main():
+	args = get_args()
+	encrypt_arg = args.encrypt
+	decrypt_arg = args.decrypt
+	aes_key = args.aes_key
+		
+	if encrypt_arg is not None:
+		show_info(encrypt_arg, True)
+		enc_message = encrypt_(encrypt_arg, aes_key)
+		show_info(enc_message)
+
+	elif decrypt_arg is not None:
+		show_info(decrypt_arg)
+		decoded = decrypt_(decrypt_arg, aes_key)
+		show_info(decoded, True)
+		if args.execute:
+			run_shellcode(decoded)
+
+	
+
+if __name__== "__main__":
+  main()
